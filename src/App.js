6a354574b6f3fd7608075e91abbd2b28aac7ccb6
jieun <balllling92@gmail.com>
2023-06-15 20:55:06 +0900

    Chore: h2태그 수정

--- src/App.js
+++ src/App.js
@@ -22,7 +22,6 @@ function App() {
 
   const [hand, setHand] = useState(initial_value);
   const [otherHand, setOtherHand] = useState(initial_value);
-  //빈배열을 초기값으로 받음. getResult 승부결과를 받아서 넘겨줌
   const [gameHistory, setGameHistory] = useState([]); 
   const [score, setScore] = useState(0);
   const [otherScore, setOtherScore] = useState(0);
@@ -40,9 +39,11 @@ function App() {
     const comparison = compareHand(nextHand, nextOtherHand);
     setHand(nextHand);
     setOtherHand(nextOtherHand);
+
     // 배열은 참조형이기때문에 state에 함수로 값을 변경할 때 새로운 값을 만들어줘야함
     // 그래서 새로운 배열을 만들어 변경해 줌
     setGameHistory([...gameHistory, nextHistoryItem]);
+
     // 스코어와 배점을 합한 값
     if (comparison > 0) setScore(score + bet);
     if (comparison < 0) setOtherScore(otherScore + bet);
@@ -77,18 +78,27 @@ function App() {
         <li>
           <Button onClick={handleClearClick}>
             <img className='reset-img' src={resetIcon} />
+            <h2 className='hidden'>초기화 버튼</h2>
           </Button>
         </li>
+
         {/* 현재 스코어 */}
         <li>
+          <h2 className='hidden'>현재 스코어</h2>
+          {/* 나의 스코어 */}
           <div className='score'>
+            <h2 className='hidden'>나의 스코어</h2>
             <div className='score-num'>
               {score}
             </div>
             <span className='score-name'>나</span>
           </div>
+
           <span className='colon'>:</span>
+
+          {/* 상대(컴퓨터)의 스코어 */}
           <div className='score'>
+            <h2 className='hidden'>상대 스코어</h2>
             <div className='score-num'>
               {otherScore}
             </div>
@@ -96,31 +106,44 @@ function App() {
           </div>
         </li>
         <li className='Box-inner'>
+
           {/* 핸드 아이콘 */}
           <div className='versus'>
+            <h2 className='hidden'>핸드 아이콘</h2>
+
+            {/* 나의 핸드 아이콘 */}
             <div className='hand'>
               <HandIcon className="hand-icon" value={hand} />
             </div>
+
             <span>vs</span>
+
+            {/* 상대(컴퓨터) 핸드 아이콘 */}
             <div className='hand'>
               <HandIcon className="hand-icon" value={otherHand} />
             </div>
           </div>
+
           {/* 배점 1부터 최대 9까지 가능 */}
           <div className='bet'>
+            <h2 className='hidden'>베점 1부터 9까지 올릴 수 있습니다</h2>
             <span>배점</span>
-            <input type="number" value={bet} min={1} max={9} onChange={handleBetChange}></input>
+            <input type="number" value={bet} min={1} max={9} onChange={handleBetChange} />
             <span>배</span>
           </div>
+
           {/* 승부기록 */}
           <div className='history'>
             <h2>승부기록</h2>
+
             {/* join - 전달받은 값을 하나의 문자열로 만들어주는 메소드 */}
             {gameHistory.join(',')}
           </div>
         </li>
+
         {/* 가위바위보 버튼 */}
         <li>
+          <h2 className='hidden'>가위바위보 선택 버튼</h2>
           <div>
             <HandButton value="rock" onClick={handleButtonClick} />
             <HandButton value="scissor" onClick={handleButtonClick} />
